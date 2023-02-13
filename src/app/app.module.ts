3afbfc101f4e66af36d7e7bd73d7004ddb970b1e
Christopher Lates <clates@crunchbase.com>
2023-02-13 13:20:16 -0500

    Example showing incompatability between Angular 14.2.2 and Google Pay 3.0.5

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { GooglePayButtonModule } from '@google-pay/button-angular';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, GooglePayButtonModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
